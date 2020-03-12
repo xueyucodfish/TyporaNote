b4a77407a66f68f33f19924a9befa04019c1103e
Your Name <you@example.com>
2020-03-12 09:44:43 +0800

    auto commit, 提交人: 薛钰, 提交时间: 2020-03-12 09:44:40

--- GitAutoPush.sh
+++ GitAutoPush.sh
@@ -8,8 +8,8 @@ git add .
 read -t 30 -p "请输入提交注释:" msg
 
 if  [ ! "$msg" ] ;then
-    echo "[commit message] 默认提交, 提交人: $(whoami), 提交时间: ${time}"
-	git commit -m "默认提交, 提交人: $(whoami), 提交时间: ${time}"
+    echo "auto commit, 提交人: $(whoami), 提交时间: ${time}"
+	git commit -m "auto commit, 提交人: $(whoami), 提交时间: ${time}"
 else
     echo "[commit message] $msg, 提交人: $(whoami), 提交时间: ${time}"
 	git commit -m "$msg, 提交人: $(whoami), 提交时间: ${time}"
